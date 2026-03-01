e706b350005a983deb762df6397b1e2d5f247586
jasoneliu <jasoneliu03@gmail.com>
2026-03-01 11:34:22 -0800

    Use pkgs for niri and tuigreet in greetd config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,18 +133,21 @@
     greetd = {
       enable = true;
 
-      settings = {
+      settings = let
+        niri-session = "${pkgs.niri}/bin/niri-session";
+        tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
+      in {
         # Launch niri on startup
         initial_session = {
           # Silence niri log: https://github.com/niri-wm/niri/issues/254
-          command = "niri-session 2> /dev/null";
+          command = "${niri-session} 2> /dev/null";
           user = "jason";
         };
 
         # Fall back to tuigreet
         default_session = {
           # Silence niri log: https://github.com/niri-wm/niri/issues/254
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session 2> /dev/null";
+          command = "${tuigreet} --time --remember --cmd ${niri-session} 2> /dev/null";
           user = "greeter";
         };
       };
