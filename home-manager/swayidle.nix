a5707aae6773ac46c14a9ee9ba92e4872b5ce498
jasoneliu <jasoneliu03@gmail.com>
2026-03-01 21:40:23 -0800

    Remove swayidle timeouts since they are handled by Noctalia

--- home-manager/swayidle.nix
+++ home-manager/swayidle.nix
@@ -3,9 +3,7 @@
   ...
 }: {
   services.swayidle = let 
-    display = status: "${pkgs.niri}/bin/niri msg action power-${status}-monitors";
     lock = "${pkgs.noctalia-shell}/bin/noctalia-shell ipc call lockScreen lock";
-    suspend = "${pkgs.systemd}/bin/systemctl suspend";
   in {
     enable = true;
 
@@ -14,20 +12,5 @@
       before-sleep = lock;
       lock = lock;
     };
-
-    timeouts = [
-      # Turn off display after 10 minutes
-      {
-        timeout = 10 * 60;
-        command = display "off";
-        resumeCommand = display "on";
-      }
-
-      # Suspend after 30 minutes
-      {
-        timeout = 30 * 60;
-        command = suspend;
-      }
-    ];
   };
 }
